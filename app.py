1494b0130f8441f8d79ad05a62c510906c1ada74
fossickanthony <139086617+fossickanthony@users.noreply.github.com>
2023-11-03 20:21:08 -0400

    Change payment domain

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def show_translator(local_dev=False):
 
 def create_checkout_session(price, quantity):
     try:
-        YOUR_DOMAIN = 'http://localhost:8502'
+        YOUR_DOMAIN = 'https://fossick.streamlit.app/'
         checkout_session = stripe.checkout.Session.create(
             line_items=[
                 {
